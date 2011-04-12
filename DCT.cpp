2e5264d5e4630cec490403e6f6b626cd4c50b2b4
nicolasrnp@gmail.com <nicolasrnp@gmail.com@7bacfe4a-4d2d-2879-b5fa-80d0502c43e4>
2011-04-12 15:25:15 +0000

    correction de l'affichage des dct avec cvShowAnyImage v2 du prof ...

--- DCT.cpp
+++ DCT.cpp
@@ -1,30 +1,11 @@
 #include "DCT.h"
 
-void razFrequence(IplImage *I, int x, int y, int taille)
-{
-	//mise a zero des hautes fréquence
-	double *data = (double*)I->imageData;
-
-	int xdeb, ydeb;
-	xdeb = ydeb = taille/2; // "taux" de précision voulu
-
-	for(int i = 0; i < taille; i++)
-	{
-		for(int j = 0; j < taille; j++)
-		{
-			if (i >= xdeb || j >= ydeb)
-			{
-				data[(i+y)*I->width + (j+x)] = 0;
-			}
-		}
-	}
-}
-
-int DCT3(IplImage *I, int taille, std::string id)
+IplImage* DCT3(IplImage *I, int taille, std::string id)
 {
     //penser a copier l'image I
     IplImage *A = cvCreateImage(cvSize(I->width, I->height), IPL_DEPTH_64F, 1);
     cvCopy(I, A);
+
     //cvCvtGray(A, A);
 
     for(int i = 0; i < A->width; i += taille)
@@ -34,15 +15,9 @@ int DCT3(IplImage *I, int taille, std::string id)
 			//cout << "(i, j) : (" << i << ", " << j << ") => (" << taille << ", " << taille << ")" << endl;
 			cvSetImageROI(A, cvRect(i, j, taille, taille));
 			cvDCT(A, A, CV_DXT_FORWARD);
-			//razFrequence(A, i, j, taille);
 			cvResetImageROI(A);
 		}
 	}
 
-	cvShowAnyImage(A, id.c_str());
-
-	cvDestroyWindow(id.c_str());
-	cvReleaseImage(&A);
-
-	return EXIT_SUCCESS;
+	return A;
 }
--- DCT.h
+++ DCT.h
@@ -8,9 +8,6 @@
 #include "lib.h"
 #include "MyImage.hpp"
 
-void razFrequence(IplImage *I, int x, int y, int taille);
-
-int DCT3(IplImage *I, int taille, std::string id);
-
+IplImage* DCT3(IplImage *I, int taille, std::string id);
 
 #endif // DCT_H_INCLUDED
